0dd82592d390d2078d9d48e99c6ed6b5ea3e1665
Roslan <120595710+qwer112358@users.noreply.github.com>
2024-09-30 20:36:30 +0500

    Update Line.cs

--- backend/Whiteboard/Models/Line.cs
+++ backend/Whiteboard/Models/Line.cs
@@ -7,7 +7,7 @@ public class Line
 	public int Id { get; set; }
 	public string? Points { get; set; } 
 	public string? Stroke { get; set; } 
-	public string? Tool { get; set; }   // Тип инструмента: pencil, rect, circle, arrow
+	public string? Tool { get; set; }   // pencil, rect, circle, arrow
 	public float? StartX { get; set; }
 	public float? StartY { get; set; }
 	public float? EndX { get; set; }
