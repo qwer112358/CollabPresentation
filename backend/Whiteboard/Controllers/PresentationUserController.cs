0b9d02a1b3ceae7468f97d4652de197b430876ab
Roslan <120595710+qwer112358@users.noreply.github.com>
2024-09-30 20:29:36 +0500

    Update PresentationUserController.cs

--- backend/Whiteboard/Controllers/PresentationUserController.cs
+++ backend/Whiteboard/Controllers/PresentationUserController.cs
@@ -20,37 +20,29 @@ namespace PresentationApp.Controllers
 		[HttpPost("{presentationId:guid}")]
 		public async Task<IActionResult> CreatePresentationUser(Guid presentationId, [FromBody] PresentationUserDto userDto)
 		{
-			// Найти презентацию с её пользователями
 			var presentation = await _dbContext.Presentations
 				.Include(p => p.PresentationUsers)
 				.ThenInclude(pu => pu.User)  // Включаем пользователей
 				.FirstOrDefaultAsync(p => p.Id == presentationId);
 
 			if (presentation == null) return NotFound("Presentation not found.");
-
-			// Найти пользователя по Nickname
 			var existingUser = await _dbContext.ApplicationUsers
 				.FirstOrDefaultAsync(u => u.Nickname == userDto.Nickname);
 
 			if (existingUser == null)
 			{
-				// Если пользователь не найден, создать нового
 				existingUser = new ApplicationUser { Id = Guid.NewGuid(), Nickname = userDto.Nickname };
 				_dbContext.ApplicationUsers.Add(existingUser);
-				await _dbContext.SaveChangesAsync(); // Сохраняем нового пользователя
+				await _dbContext.SaveChangesAsync();
 			}
 
-			// Проверить, существует ли уже связь PresentationUser
 			var existingPresentationUser = presentation.PresentationUsers
 				.FirstOrDefault(pu => pu.UserId == existingUser.Id);
 
 			if (existingPresentationUser != null)
 			{
-				// Если пользователь уже есть в презентации, возвращаем его
 				return Conflict("User is already a member of this presentation.");
 			}
-
-			// Если связи нет, добавляем нового PresentationUser
 			var newPresentationUser = new PresentationUser
 			{
 				UserId = existingUser.Id,
@@ -59,8 +51,6 @@ namespace PresentationApp.Controllers
 				Role = userDto.Role
 			};
 			presentation.PresentationUsers.Add(newPresentationUser);
-
-			// Добавляем в контекст и сохраняем
 			_dbContext.PresentationUsers.Add(newPresentationUser);
 			await _dbContext.SaveChangesAsync();
 
